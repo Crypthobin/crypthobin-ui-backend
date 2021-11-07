64464d8cb2b5e111f6d80945b5629d70a2fb3d31
Park Min Hyeok <pmh_only@pmh.codes>
2021-11-07 15:01:06 +0900

    feat: add `qrcode` endpoint
    Signed-off-by: Park Min Hyeok <pmh_only@pmh.codes>

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import morgan from 'morgan'
 import express from 'express'
 import authRouter from './routers/auth'
+import { qrGenerator } from './utils/qr'
 import walletRouter from './routers/wallets'
 import addressRouter from './routers/addresses'
 import notfound from './middlewares/notfound'
@@ -11,6 +12,7 @@ app.use(morgan('common'))
 app.use('/auth', authRouter)
 app.use('/api/wallets', walletRouter)
 app.use('/api/addresses', addressRouter)
+app.get('/api/qr/:addr.png', qrGenerator)
 app.use(notfound)
 
 app.listen(3000, () => {
